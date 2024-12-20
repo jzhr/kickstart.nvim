700d73de8a196ff76e5c111eba1a5a5f3d95ac4e
Jason Zheng <jasnzhng@gmail.com>
2024-12-19 22:13:17 -0500

    add fugitive keymap, theming

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -20,7 +20,7 @@ return {
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
-  { 'tpope/vim-fugitive' },
+  { 'tpope/vim-fugitive', vim.keymap.set('n', '<leader>g', ':0G<CR>', { desc = 'Fu[g]itive' }) },
   {
     'stevearc/oil.nvim',
     ---@module 'oil'
@@ -30,5 +30,18 @@ return {
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+  },
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_transparent_background = 1
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
   },
 }
