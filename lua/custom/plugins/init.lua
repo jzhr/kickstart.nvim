7ea4d508ed80f5c09a55d32996861097825f2555
Jason Zheng <jasnzhng@gmail.com>
2025-01-01 21:53:01 -0500

    fzf, TS comments, and autotagging

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -44,4 +44,41 @@ return {
       vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
+  {
+    'windwp/nvim-ts-autotag',
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+  {
+    'folke/ts-comments.nvim',
+    opts = {},
+    event = 'VeryLazy',
+    enabled = vim.fn.has 'nvim-0.10.0' == 1,
+  },
+  {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    opts = {},
+    config = function()
+      require('fzf-lua').setup {
+        -- Add your fzf-lua configurations here
+        winopts = {
+          height = 0.85,
+          width = 0.80,
+          border = 'rounded',
+        },
+      }
+    end,
+    keys = {
+      { '<leader>sf', "<cmd>lua require('fzf-lua').files()<CR>", desc = 'Find Files (fzf-lua)' },
+      { '<leader>sg', "<cmd>lua require('fzf-lua').live_grep()<CR>", desc = 'Live Grep (fzf-lua)' },
+      { '<leader>sb', "<cmd>lua require('fzf-lua').buffers()<CR>", desc = 'Find Buffers (fzf-lua)' },
+      { '<leader>sh', "<cmd>lua require('fzf-lua').help_tags()<CR>", desc = 'Find Help (fzf-lua)' },
+    },
+  },
 }
